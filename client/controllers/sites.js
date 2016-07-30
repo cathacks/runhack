06a37dec0f97f67436fb26c9f8277d5ba8f705ab
Tom Saunders <tom@tsaunders.net>
2016-07-30 17:33:47 +1000

    Add graffiti sites data
    for the lulz

--- client/controllers/sites.js
+++ client/controllers/sites.js
@@ -2,51 +2,39 @@ var sites = {
 	init:     function($div) {
 		this.$div = $div;
 		this.$div.append("<h3>Points of interest</h3>");
-		this.$div.css('height', '400px').css('border', '1px solid black');
+		this.$div.css('min-height', '400px').css('border', '1px solid black');
 
 		this.$tablist = $("<div class='panel-group' id='typelist' role='tablist'></div>");
-
-		var pretend = ['Alpha', 'Beta', 'Gamma'];
-		for (var i = 0; i < pretend.length; i++) {
-
-		}
-
 		this.$tablist.appendTo(this.$div);
 
 		this.loadData();
 	},
 	loadData: function() {
+		var tokenQ = '?$$app_token=vY4SluUWF98mpICqPqqkhBxGE';
+		var pubArt = 'https://www.data.act.gov.au/resource/s538-zqvb.json';
+		var graffiti = 'https://www.data.act.gov.au/resource/ppyi-zp42.json';
 
-		var silentCarto = window.theSilentCartographer
+		//each item needs to have the following keys : label, lat and lng
+		$.get(pubArt + tokenQ, function(data, status, xhr) {
+			this.addPanel('Public Art', data.map(function(item) {
+				item.label = item.title;
+				item.lat   = -item.longitude.coordinates[1];
+				item.lng   = item.longitude.coordinates[0];
+				return item;
+			}));
+		}.bind(this), 'json');
 
-
-		var pubArt = 'https://www.data.act.gov.au/resource/s538-zqvb.json?$$app_token=vY4SluUWF98mpICqPqqkhBxGE';
-
-		var self = this;
-
-		$.get(pubArt, function(data, status, xhr) {
-			data = data.map(function(art) {
-				art.location = art.longitude.coordinates;
-				var lat      = -art.location[1];
-				var lng      = art.location[0];
-				console.log(art.title, art.suburb, lat, lng);
-
-				//interface
-				art.label = art.title;
-				art.lat   = -art.location[1];
-				art.lng   = art.location[0];
-
-				silentCarto.addMarker(art.lat, art.lng, art.label);
-
-				return art;
-			});
-
-			self.addPanel('Public Art', data);
-		}, 'json');
+		$.get(graffiti + tokenQ, function(data, status, xhr) {
+			this.addPanel('Graffiti Sites', data.map(function(item) {
+				item.label = item.location;
+				item.lat   = item.latitude;
+				item.lng   = item.longitude;
+				return item;
+			}));
+		}.bind(this), 'json');
 	},
 
 	addPanel: function(title, data) {
-		console.log('add panel', title, data);
 		var key = title.toLowerCase().replace(/ /g, '_'); //lowercase and underscore all spaces
 
 		//TODO handlebars template would be cleaner?
@@ -67,6 +55,8 @@ var sites = {
 		data.map(function(item) {
 			var li = "<li class='list-group-item' data-lat='" + item.lat + "' data-lng='" + item.lng + "'>" + item.label + '</li>';
 			$list.append(li);
+
+			theSilentCartographer.addMarker(item.lat, item.lng, item.label);
 
 			return item;
 		});
