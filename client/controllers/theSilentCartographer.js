7bf8c2b19b962e95ec5c9dc6d7c4930f132f9487
Nick Sonneveld <nick@sonneveld.name>
2016-07-30 15:50:09 +1000

    add map

--- client/controllers/theSilentCartographer.js
+++ client/controllers/theSilentCartographer.js
@@ -3,5 +3,21 @@ var theSilentCartographer = {
 		this.$div = $div;
 		this.$div.append("<h1>Map</h1>");
 		this.$div.css('height', '400px').css('border', '1px solid black');
+		this.$div.append("<div id='map'></div>");
+
+		map = new google.maps.Map(document.getElementById('map'), {
+		  center: {lat: -34.397, lng: 150.644},
+		  zoom: 8
+		});
 	}
 };
+
+
+var map;
+function initMap() {
+	// called when google maps is loaded.
+	// map = new google.maps.Map(document.getElementById('map'), {
+	//   center: {lat: -34.397, lng: 150.644},
+	//   zoom: 8
+	// });
+}
