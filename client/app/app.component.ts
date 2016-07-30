4067cffd3f9bd8d9f93533f5df120a926cd0dc52
Nick Sonneveld <nick@sonneveld.name>
2016-07-30 13:10:03 +1000

    wip, style tidy up

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,6 +1,27 @@
 import { Component } from '@angular/core';
+
+export class Runner {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{runner.name}} details!</h2>
+    <div><label>id: </label>{{runner.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="runner.name" placeholder="name">
+    </div>
+    `
 })
-export class AppComponent { }
+
+export class AppComponent {
+  title = 'RunHack';
+  runner: Runner = {
+    id: 1,
+    name: 'Gary'
+  };
+}
