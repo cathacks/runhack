e1c7f817bcdd6dce188badec68439302a8c0c535
Chris Mitchell <chris@obfuscated.cx>
2016-07-31 12:42:39 +1000

    Fixed footer and added govhack logo

--- client/controllers/footer.js
+++ client/controllers/footer.js
@@ -1,7 +1,7 @@
 var footer = {
 	init: function($div){
 		this.$div = $div;
-		this.$div.append("<p>Fun Run CBR by Cathacks</p>");
+		this.$div.append("<img src='images/govhack.png' height='50'/><p>Fun Run CBR by Cathacks</p>");
 		//this.$div.css('height', '100px').css('border', '1px solid black');
 	}
 };
