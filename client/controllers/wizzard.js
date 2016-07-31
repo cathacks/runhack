cc968e2896153631f121a69f4f44a8fd3621a9bc
Nick Sonneveld <nick@sonneveld.name>
2016-07-31 12:07:01 +1000

    wizzard wip

--- client/controllers/wizzard.js
+++ client/controllers/wizzard.js
@@ -5,35 +5,37 @@ var wizzard = {
 		this.$div.css('height', '400px').css('border', '1px solid black');
 
 		this.$div.append("<p>A basic start location</p>")
-		this.$div.append('<input id="goaddress" type="text" value="Parliament House, ACT">')
+		this.$div.append('<input id="startaddress" type="text" value="Parliament House, ACT">')
 
-		this.$div.append("<p>Radius (km)</p>")
-		this.$div.append('<input id="goradius" type="range" min="1" max="10" step="1" />')
+		// this.$div.append("<p>Radius (km)</p>")
+		// this.$div.append('<input id="goradius" type="range" min="1" max="10" step="1" />')
+
+		this.$div.append("<p>A basic end location</p>")
+		this.$div.append('<input id="endaddress" type="text" value="Grease Monkey, ACT">')
+
+		this.$div.append("<p>or loop to start</p>")
+		this.$div.append('<input id="loopaddress" type="checkbox" value="true">')
 
 		this.$div.append("<br>")
 		this.$div.append('<button id="gobutton" name="button">Click me</button>')
 
 		$("#gobutton").click(function() {
 
-			var addr = $("#goaddress").val();
-			var radius = parseInt($("#goradius").val());
+			var startAddr = $("#startaddress").val();
+			var endAddr = $("#endaddress").val();
 
-			theSilentCartographer.geocoder.geocode( { 'address': addr}, function(results, status) {
-			  if (status == 'OK') {
-				// theSilentCartographer.map.setCenter(results[0].geometry.location);
-
-				theSilentCartographer.addSearchCircle(results[0].geometry.location, radius);
-				// var marker = new google.maps.Marker({
-				// 	map: map,
-				// 	position: results[0].geometry.location
-				// });
-
-
-
-			  } else {
-				alert('Geocode was not successful for the following reason: ' + status);
-			  }
-			});
+			var loopAddr = $("#loopaddress").is(':checked')
+			
+			// var radius = parseInt($("#goradius").val());
+			// theSilentCartographer.geocoder.geocode( { 'address': addr}, function(results, status) {
+			  // if (status == 'OK') {
+				// theSilentCartographer.addSearchCircle(results[0].geometry.location, radius);
+				//  find x random sites within radius
+				//
+			  // } else {
+				// alert('Geocode was not successful for the following reason: ' + status);
+			  // }
+			// });
 
 
 		});
