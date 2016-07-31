446976f5f6f453fb77b163e5a094a5a788cadda5
Nick Sonneveld <nick@sonneveld.name>
2016-07-31 12:37:04 +1000

    wip routing

--- client/controllers/wizzard.js
+++ client/controllers/wizzard.js
@@ -7,13 +7,10 @@ var wizzard = {
 		this.$div.append("<p>A basic start location</p>")
 		this.$div.append('<input id="startaddress" type="text" value="Parliament House, ACT">')
 
-		// this.$div.append("<p>Radius (km)</p>")
-		// this.$div.append('<input id="goradius" type="range" min="1" max="10" step="1" />')
+		// this.$div.append("<p>A basic end location</p>")
+		// this.$div.append('<input id="endaddress" type="text" value="Grease Monkey, ACT">')
 
-		this.$div.append("<p>A basic end location</p>")
-		this.$div.append('<input id="endaddress" type="text" value="Grease Monkey, ACT">')
-
-		this.$div.append("<p>or loop to start</p>")
+		this.$div.append("<p>loop to start</p>")
 		this.$div.append('<input id="loopaddress" type="checkbox" value="true">')
 
 		this.$div.append("<br>")
@@ -22,25 +19,30 @@ var wizzard = {
 		$("#gobutton").click(function() {
 
 			var startAddr = $("#startaddress").val();
-			var endAddr = $("#endaddress").val();
+			// var endAddr = $("#endaddress").val();
 
 			var loopAddr = $("#loopaddress").is(':checked')
+
+			theSilentCartographer.setLoop(loopAddr);
 			
-			// var radius = parseInt($("#goradius").val());
-			// theSilentCartographer.geocoder.geocode( { 'address': addr}, function(results, status) {
-			  // if (status == 'OK') {
-				// theSilentCartographer.addSearchCircle(results[0].geometry.location, radius);
-				//  find x random sites within radius
-				//
-			  // } else {
-				// alert('Geocode was not successful for the following reason: ' + status);
-			  // }
+			theSilentCartographer.geocoder.geocode( { 'address': startAddr}, function(results, status) {
+			  if (status == 'OK') {
+			  	theSilentCartographer.setStart(results[0].geometry.location);
+			  } else {
+				alert('Geocode was not successful for the following reason: ' + status);
+			  }
+
+			  theSilentCartographer.updateRoute();
+			});
+
+			// theSilentCartographer.geocoder.geocode( { 'address': endAddr}, function(results, status) {
+			//   if (status == 'OK') {
+			//   	theSilentCartographer.setEnd(results[0].geometry.location);
+			//   } else {
+			// 	alert('Geocode was not successful for the following reason: ' + status);
+			//   }
 			// });
 
-
 		});
-
-
-
 	}
 };
